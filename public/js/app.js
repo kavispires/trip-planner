630be3b5df538f6b1d924c39b889a96ec1b98f43
Jonathan D. Mann <jdmann84@gmail.com>
2017-02-01 08:14:01 -0700

    created branch "jonathan" and testing push to branch

--- public/js/app.js
+++ public/js/app.js
@@ -1,75 +1,75 @@
-   function initMap() {
-        // Styles a map in night mode.
-        var map = new google.maps.Map(document.getElementById('googleMap'), {
-          center: {lat: 40.674, lng: -73.945},
-          zoom: 12,
-          styles: [
+function initMap() {
+    // Styles a map in night mode.
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+      center: {lat: 40.674, lng: -73.945},
+      zoom: 12,
+      styles: [
+        {
+          "featureType": "landscape",
+          "elementType": "geometry.fill",
+          "stylers": [
             {
-              "featureType": "landscape",
-              "elementType": "geometry.fill",
-              "stylers": [
-                {
-                  "color": "#e4e2e0"
-                }
-              ]
-            },
-            {
-              "featureType": "poi",
-              "elementType": "geometry.fill",
-              "stylers": [
-                {
-                  "color": "#cdd1cc"
-                }
-              ]
-            },
-            {
-              "featureType": "poi.park",
-              "elementType": "labels.text.fill",
-              "stylers": [
-                {
-                  "color": "#07889b"
-                }
-              ]
-            },
-            {
-              "featureType": "road",
-              "elementType": "labels.icon",
-              "stylers": [
-                {
-                  "saturation": -70
-                }
-              ]
-            },
-            {
-              "featureType": "road.highway",
-              "elementType": "geometry.fill",
-              "stylers": [
-                {
-                  "color": "#e37222"
-                }
-              ]
-            },
-            {
-              "featureType": "road.highway",
-              "elementType": "geometry.stroke",
-              "stylers": [
-                {
-                  "color": "#e37222"
-                }
-              ]
-            },
-            {
-              "featureType": "water",
-              "elementType": "geometry.fill",
-              "stylers": [
-                {
-                  "color": "#61b5ba"
-                }
-              ]
+              "color": "#e4e2e0"
             }
           ]
-        });
-      }
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#cdd1cc"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#07889b"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "saturation": -70
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#e37222"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#e37222"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#61b5ba"
+            }
+          ]
+        }
+      ]
+    });
+}
 
 
 $(document).ready(function() {
